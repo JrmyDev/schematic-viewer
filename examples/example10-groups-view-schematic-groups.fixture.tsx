921da12a58fd7a96e0d0ef29ed514a477bda9733
Arnav K <arnavkaushal09@gmail.com>
2025-09-01 02:41:19 +0530

    ffd

--- examples/example10-groups-view-schematic-groups.fixture.tsx
+++ examples/example10-groups-view-schematic-groups.fixture.tsx
@@ -60,7 +60,7 @@ const circuitJson = renderToCircuitJson(
 
 export default () => {
   return (
-    <div style={{ position: "relative", height: "100%" }}>
+    <div style={{ position: "relative", width: "600vw", height: "600vh" }}>
       <SchematicViewer
         circuitJson={circuitJson}
         containerStyle={{
